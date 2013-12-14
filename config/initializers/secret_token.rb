53c945e8d28407e5c634cc845784e8b35939bbbd
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-12-14 13:12:24 +0900

    rails new -T; ignore /vendor/bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IgakutenApp::Application.config.secret_key_base = 'a36803079c4c3c0c40d2028ab06a910f854909ebdaa374151c8bb214a817613753a9086ca54546558d8723ca53179f84c2a9e93ba9e5a48da69d5fe4e0dab6ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IgakutenApp::Application.config.session_store :cookie_store, key: '_igakuten_app_session'
