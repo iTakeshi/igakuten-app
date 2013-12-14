3a29c8e01eb1c8e72150e158adabbcf0dcd18afa
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-12-14 22:57:05 +0900

    set default_url[:host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ IgakutenApp::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # ActionMailer::Base.default_url_options
+  config.action_mailer.default_url_options = { host: 'app.hokudai-igakuten.org' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
