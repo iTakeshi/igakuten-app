c0511c49460bb6c4753a085086f09d198dc21e68
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-12-21 15:40:12 +0900

    add validation to quorum

--- app/models/quorum.rb
+++ app/models/quorum.rb
@@ -2,6 +2,9 @@ class Quorum < ActiveRecord::Base
   belongs_to :period
   belongs_to :team
 
+  validates :period_id do
+    uniqueness scope: :team_id
+  end
   validates :quorum do
     presence
   end
