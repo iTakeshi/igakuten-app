afdce445d1050ef49d192a51a94975132737bb5e
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-12-22 16:18:53 +0900

    add validations

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,15 @@ class Shift < ActiveRecord::Base
   belongs_to :period
   belongs_to :team
   belongs_to :staff
+
+  validates :period_id do
+    presence
+    uniqueness scope: :staff_id
+  end
+  validates :team_id do
+    presence
+  end
+  validates :staff_id do
+    presence
+  end
 end
--- app/models/unavailable_period.rb
+++ app/models/unavailable_period.rb
@@ -1,4 +1,12 @@
 class UnavailablePeriod < ActiveRecord::Base
   belongs_to :staff
   belongs_to :period
+
+  validates :staff_id do
+    presence
+    uniqueness scope: :period_id
+  end
+  validates :period_id do
+    presence
+  end
 end
