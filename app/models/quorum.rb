e292f4b06eef4d779d89e80944417506f2ba3dfe
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-01-03 17:32:02 +0900

    remove less_than_or_equal_to validation on Quorum#quorum

--- app/models/quorum.rb
+++ app/models/quorum.rb
@@ -8,7 +8,6 @@ class Quorum < ActiveRecord::Base
   validates :quorum do
     presence
     numericality only_integer: true,
-                 greater_than_or_equal_to: 0,
-                 less_than_or_equal_to: 20
+                 greater_than_or_equal_to: 0
   end
 end
