0c2335326a1f19d9a6961eb0b58a4646ec663dff
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-01 20:18:35 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@ if Team.where(name: '休憩').count == 0
               name: '休憩',
               display_order: 1
 end
+
+if MailingList.where(account_name: 'all').count == 0
+  MailingList.create account_name: 'all'
+end
+
+team_recess = Team.where(name: '休憩').first
+all = MailingList.where(account_name: 'all').first
+unless team_recess.mailing_lists.map(&:account_name).include? 'all'
+  team_recess.mailing_lists << all
+end
