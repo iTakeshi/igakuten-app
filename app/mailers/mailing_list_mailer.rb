6af65f03a5197b9d670df64d9814f5d2a301e7a6
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-16 22:46:54 +0900

    debug smtp settings

--- app/mailers/mailing_list_mailer.rb
+++ app/mailers/mailing_list_mailer.rb
@@ -1,14 +1,21 @@
 class MailingListMailer < ActionMailer::Base
+  after_action :set_delivery_options
+
   def publication(to_addresses, ml_email, sender_name, sender_email, subject, body)
     @ml_email = ml_email
     @sender_name = sender_name
     @sender_email = sender_email
     @body = body
 
-    delivery_options = {
+    mail to: to_addresses, from: ml_email, reply_to: sender_email, subject: subject
+  end
+
+  private
+
+  def set_delivery_options
+    mail.delivery_method.settings.merge!({
       user_name: Figaro.env.ml_smtp_user,
       password: Figaro.env.ml_smtp_pass
-    }
-    mail to: to_addresses, from: ml_email, reply_to: sender_email, subject: subject
+    })
   end
 end
