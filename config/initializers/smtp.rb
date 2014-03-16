684f0e8e6c5cfc95a8b643e95f079e997498d82c
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-16 17:25:09 +0900

    SMTP setting

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,10 +2,8 @@ IgakutenApp::Application.configure do
   if Rails.env == 'production'
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address: 'smtp-mail.outlook.com',
-      port: 25,
-      enable_starttls_auto: true,
-      authentification: :plain,
+      address: 'smtp.mandrillapp.com',
+      port: 587,
       user_name: Figaro.env.smtp_user,
       password: Figaro.env.smtp_pass
     }
