29bf658dfc84bf48c112374568d1ccaad17a615b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-16 23:37:58 +0900

    debug mailer

--- app/mailers/mailing_list_mailer.rb
+++ app/mailers/mailing_list_mailer.rb
@@ -1,23 +1,17 @@
 class MailingListMailer < ActionMailer::Base
-  after_action :set_delivery_options
+  self.smtp_settings = {
+    address:   'smtp.mandrillapp.com',
+    port:      587,
+    user_name: Figaro.env.ml_smtp_user,
+    password:  Figaro.env.ml_smtp_pass
+  }
 
   def publication(to_addresses, ml_email, sender_name, sender_email, message_subject, message_body)
     @ml_email = ml_email
     @sender_name = sender_name
     @sender_email = sender_email
-    @message_subject = message_subject
     @message_body = message_body
 
-    mail to: to_addresses, from: ml_email, reply_to: sender_email
-  end
-
-  private
-
-  def set_delivery_options
-    mail.delivery_method.settings.merge!({
-      user_name: Figaro.env.ml_smtp_user,
-      password: Figaro.env.ml_smtp_pass
-    })
-    mail.subject = @message_subject
+    mail to: to_addresses, from: ml_email, reply_to: sender_email, subject: message_subject
   end
 end
