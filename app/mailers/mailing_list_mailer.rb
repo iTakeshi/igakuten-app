59d708ff516df4460f184d60dadaeb820e603d5d
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-16 23:19:32 +0900

    fix mail subject

--- app/mailers/mailing_list_mailer.rb
+++ app/mailers/mailing_list_mailer.rb
@@ -5,9 +5,10 @@ class MailingListMailer < ActionMailer::Base
     @ml_email = ml_email
     @sender_name = sender_name
     @sender_email = sender_email
+    @message_subject = message_subject
     @message_body = message_body
 
-    mail to: to_addresses, from: ml_email, reply_to: sender_email, subject: message_subject
+    mail to: to_addresses, from: ml_email, reply_to: sender_email
   end
 
   private
@@ -17,5 +18,6 @@ class MailingListMailer < ActionMailer::Base
       user_name: Figaro.env.ml_smtp_user,
       password: Figaro.env.ml_smtp_pass
     })
+    mail.subject = @message_subject
   end
 end
