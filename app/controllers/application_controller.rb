dee87780b93225d26333911bdc54a9b2a2142f08
iTakeshi <takeshi.ito.doraemon@gmail.com>
2014-03-17 20:08:36 +0900

    extract authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  http_basic_authenticate_with name: Figaro.env.http_user, password: Figaro.env.http_pass
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Figaro.env.http_user && password == Figaro.env.http_pass
+    end
+  end
 end
